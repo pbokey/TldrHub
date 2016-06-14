3f4c9d1bb40766e136999831439feab6071be502
pbokey <pranavbokey@Pranavs-MacBook-Pro.local>
2016-06-14 16:45:07 -0400

    Revert "reverting back to two weeks ago"
    This reverts commit 5dc5da52f9e2fa06eecdb83e043f72917f032487.

--- js/script.js
+++ js/script.js
@@ -6,22 +6,6 @@ $(window).scroll(function() {
   }
 });
 
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-	$('.logo-white').css("display","none");
-  } else {
-	$('.logo-white').css("display","block");
-  }
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-	$('.logo-dark').css("display","block");
-  } else {
-	$('.logo-dark').css("display","none");
-  }
-});
-
 $(document).ready(function(){
     $(".search-button").click(function(){
         $(".search-bar").toggleClass("search-on");
