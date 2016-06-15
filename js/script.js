d8e23dadf9ac4708325d15b255fd5cdffcda2902
Nathan Morse <nathan.a.morse@gmail.com>
2016-06-15 18:11:40 -0400

    new navbar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 50) {
+	$('.logo-img').addClass('logo-on');
+	$('.logo-img').css('padding','10px 15px');
+  } else {
+	$('.logo-img').removeClass('logo-on');
+	$('.logo-img').css('padding','0');
+  }
+});
 
 $(document).ready(function(){
     $(".search-button").click(function(){
