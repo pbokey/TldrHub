8d16f002f45397e6165b68efe0a2edd12ce0be4e
Nathan Morse <nathan.a.morse@gmail.com>
2016-06-15 12:49:34 -0400

    more changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-	$('nav').addClass('shrink');
-  } else {
-	$('nav').removeClass('shrink');
-  }
-});
+
 
 $(document).ready(function(){
     $(".search-button").click(function(){
