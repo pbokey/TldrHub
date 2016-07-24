5dc4edeb851fb3a6f971db2db64705789b689c7d
pbokey <Pranav@Pranavs-MacBook-Pro.local>
2016-07-24 17:27:49 -0400

    added tldr link and changed the color

--- bootstrap/js/bootstrap.js
+++ bootstrap/js/bootstrap.js
@@ -2366,5 +2366,9 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-
+  $('.collapse').on('shown.bs.collapse', function(){
+    $(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+    }).on('hidden.bs.collapse', function(){
+    $(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+    });
 
