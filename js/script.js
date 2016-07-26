0c3efc0fba0d7990b018f6483021b847be5e29d7
pbokey <Pranav@Pranavs-MacBook-Pro.local>
2016-07-25 23:37:25 -0400

    no idea

--- js/script.js
+++ js/script.js
@@ -1,34 +1,31 @@
 $(window).scroll(function() {
   if ($(document).scrollTop() > 89) {
-	$('.info').css('margin-top','0');
+  $('.info').css('margin-top','0');
   } else {
-	$('.info').css('margin-top','-52px');
+  $('.info').css('margin-top','-52px');
   }
 });
 
 $(window).scroll(function() {
   if ($(document).scrollTop() > 89) {
-	$('.navbar-fixed-top').addClass('shrink');
-  } else {
-	$('.navbar-fixed-top').removeClass('shrink');
-  }
-});
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 400) {
-  $('.bigpic-heading').css('background','rgba(0,0,0,.9)');
-  } else if ($(document).scrollTop() < 400) {
-    $('.bigpic-heading').css('background', 'rgba(0,0,0,.7)');
-  }
-  else {
-
+  $('.navbar-fixed-top').addClass('shrink');
+  } else  if ($(document).scrollTop() > 89) {
+  $('.navbar-fixed-top').removeClass('shrink');
   }
 });
 
 $(document).ready(function(){
     $(".search-button").click(function(){
         $(".search-bar").toggleClass("search-on");
-		$(".search-button").toggleClass("search-on");
+    $(".search-button").toggleClass("search-on");
     });
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 200) {
+  $('.bigpic-heading').css('background','rgba(0,0,0,.85)');
+  } else if ($(document).scrollTop() < 200) {
+    $('.bigpic-heading').css('background', 'rgba(0,0,0,.6)');
+  }
 });
 
